514e71ad411f3e1cf3f18b40c99da432e8782250
dylanclarke890 <dylanclarke890@gmail.com>
2021-12-24 14:54:28 +0000

    added turnstate to keep track of turn specific info

---
+++ CardsAndMonsters.Models/TurnState.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace CardsAndMonsters.Models
+{
+    public class TurnState : BaseModel
+    {
+        public TurnState()
+        {
+            NormalSummonLimit = 1;
+            NormalSummonedMonsters = new List<Monster>();
+        }
+
+        public int NormalSummonLimit { get; set; }
+
+        public IList<Monster> NormalSummonedMonsters { get; set; }
+    }
+}
