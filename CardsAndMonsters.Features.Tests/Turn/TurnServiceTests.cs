52a14d1d5a89d99cfc467ed1ea5f1351afc310a5
dylanclarke890 <dylanclarke890@gmail.com>
2022-01-08 16:55:47 +0000

    added turn service tests

--- CardsAndMonsters.Features.Tests/Turn/TurnServiceTests.cs
+++ CardsAndMonsters.Features.Tests/Turn/TurnServiceTests.cs
@@ -3,6 +3,8 @@ using CardsAndMonsters.Features.Logging;
 using CardsAndMonsters.Features.Turn;
 using CardsAndMonsters.Features.TurnPhase;
 using CardsAndMonsters.Models;
+using CardsAndMonsters.Models.Cards;
+using CardsAndMonsters.Models.Enums;
 using Moq;
 using System.Threading.Tasks;
 using Xunit;
@@ -33,49 +35,129 @@ namespace CardsAndMonsters.Features.Tests.Turn
         }
 
         [Fact]
-        public async Task StartTurn_StateUnderTest_ExpectedBehavior()
+        public async Task StartTurn_DrawCardFalse_CompletesSuccessfully()
         {
             // Arrange
+            Board board = new();
+            Duelist duelist = new("test");
+
+            _mockPhaseService.Setup(ps => ps.EnterPhase(Phase.Standby, board)).Returns(Task.CompletedTask);
+            _mockPhaseService.Setup(ps => ps.EnterPhase(Phase.Main, board)).Returns(Task.CompletedTask);
+
             var service = CreateService();
-            Duelist duelist = null;
             bool drawCard = false;
-            Board board = null;
 
             // Act
             await service.StartTurn(duelist, drawCard, board);
 
             // Assert
-            Assert.True(false);
+            Assert.Equal(0, board.CurrentTurn.CardsDrawn);
+
             _mockRepository.VerifyAll();
         }
 
         [Fact]
-        public async Task ResumeTurn_StateUnderTest_ExpectedBehavior()
+        public async Task StartTurn_DrawCardTrue_EndsGameIfDrawingFails()
         {
             // Arrange
+            Board board = new();
+            Duelist duelist = new("test");
+
+            _mockPhaseService.Setup(ps => ps.EnterPhase(Phase.Standby, board))
+                .Returns(Task.CompletedTask);
+            _mockDuelLogService.Setup(ps => ps.AddNewEventLog(Event.DrawCard, duelist));
+            _mockGameOverService.Setup(gos => gos.EndGame(duelist, LossReason.DeckOut))
+                .Returns(Task.CompletedTask);
+
             var service = CreateService();
-            Board board = null;
+            bool drawCard = true;
+
+            // Act
+            await service.StartTurn(duelist, drawCard, board);
+
+            // Assert
+            Assert.Equal(1, board.CurrentTurn.CardsDrawn);
+
+            _mockRepository.VerifyAll();
+        }
+
+        [Fact]
+        public async Task StartTurn_DrawCardTrue_EntersMainPhaseIfDrawingSuccess()
+        {
+            // Arrange
+            Board board = new();
+            Duelist duelist = new("test");
+            duelist.Deck.Add(new Monster(100, 100));
+
+            _mockPhaseService.Setup(ps => ps.EnterPhase(Phase.Standby, board))
+                .Returns(Task.CompletedTask);
+            _mockPhaseService.Setup(ps => ps.EnterPhase(Phase.Main, board))
+                .Returns(Task.CompletedTask);
+            _mockDuelLogService.Setup(ps => ps.AddNewEventLog(Event.DrawCard, duelist));
+
+            var service = CreateService();
+            bool drawCard = true;
+
+            // Act
+            await service.StartTurn(duelist, drawCard, board);
+
+            // Assert
+            Assert.Equal(1, board.CurrentTurn.CardsDrawn);
+
+            _mockRepository.VerifyAll();
+        }
+
+        [Theory]
+        [InlineData(Phase.Standby)]
+        [InlineData(Phase.Main)]
+        [InlineData(Phase.Battle)]
+        [InlineData(Phase.End)]
+        public async Task ResumeTurn_DifferentPhaseParameters_ResumesLastPhase(Phase phase)
+        {
+            // Arrange
+            Board board = new() { CurrentTurn = new() { Phase = phase } };
+
+            _mockPhaseService.Setup(ps => ps.EnterPhase(phase, board))
+                .Returns(Task.CompletedTask);
+            
+            var service = CreateService();
 
             // Act
             await service.ResumeTurn(board);
 
             // Assert
-            Assert.True(false);
+
             _mockRepository.VerifyAll();
         }
 
         [Fact]
-        public async Task EndTurn_StateUnderTest_ExpectedBehavior()
+        public async Task EndTurn_ValidBoardState_IncrementsTurnCountCorrectly()
         {
             // Arrange
+            Duelist player = new("test");
+            Duelist opponent = new("testopponent");
+            Board board = new(player, opponent) { CurrentTurn = new() { Duelist = player } };
+            player.Deck.Add(new Monster(100, 100));
+            opponent.Deck.Add(new Monster(100, 100));
+
+            _mockPhaseService.Setup(ps => ps.EnterPhase(Phase.End, board))
+                .Returns(Task.CompletedTask);
+            _mockPhaseService.Setup(ps => ps.EnterPhase(Phase.Standby, board))
+                .Returns(Task.CompletedTask);
+            _mockPhaseService.Setup(ps => ps.EnterPhase(Phase.Main, board))
+                .Returns(Task.CompletedTask);
+
+            _mockDuelLogService.Setup(ps => ps.AddNewEventLog(Event.TurnChange, player));
+            _mockDuelLogService.Setup(ps => ps.AddNewEventLog(Event.DrawCard, opponent));
+
             var service = CreateService();
-            Board board = null;
 
             // Act
             await service.EndTurn(board);
 
             // Assert
-            Assert.True(false);
+            Assert.Equal(1, board.TurnCount);
+
             _mockRepository.VerifyAll();
         }
     }
