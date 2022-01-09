905dd6da34d97403147889c1c51d9057a4924536
dylanclarke890 <dylanclarke890@gmail.com>
2022-01-09 14:42:16 +0000

    added error checks to position service

--- CardsAndMonsters.Features/Position/PositionService.cs
+++ CardsAndMonsters.Features/Position/PositionService.cs
@@ -1,8 +1,8 @@
-﻿using CardsAndMonsters.Features.Logging;
+﻿using CardsAndMonsters.Core.Exceptions;
+using CardsAndMonsters.Features.Logging;
 using CardsAndMonsters.Models;
 using CardsAndMonsters.Models.Cards;
 using CardsAndMonsters.Models.Enums;
-using System;
 using System.Linq;
 
 namespace CardsAndMonsters.Features.Position
@@ -24,15 +24,30 @@ namespace CardsAndMonsters.Features.Position
                 FieldPosition.HorizontalDown => FieldPosition.VerticalUp,
                 FieldPosition.VerticalUp => FieldPosition.HorizontalUp,
                 FieldPosition.VerticalDown => FieldPosition.VerticalUp,
-                _ => throw new ArgumentException("Couldn't work out the new position")
+                _ => throw new GameArgumentException<BaseCard>(nameof(old), old)
             };
         }
 
         public void PositionSwitched(Monster monster, Board board)
         {
+            if (monster == null)
+            {
+                throw new GameArgumentException<Monster>(nameof(monster), monster);
+            }
+            if (board == null)
+            {
+                throw new GameArgumentException<Monster>(nameof(board), board);
+            }
+
             var currentMonster = board.PlayerField.Monsters.FirstOrDefault(m => m.Equals(monster));
+            if (currentMonster == null)
+            {
+                throw new GameArgumentException<Monster>(nameof(currentMonster), currentMonster);
+            }
+
             board.PlayerField.Monsters[board.PlayerField.Monsters.IndexOf(currentMonster)] = monster;
             board.CurrentTurn.MonsterState[monster.Id].AbleToSwitch = false;
+
             _duelLogService.AddNewEventLog(Event.MonsterPositionChange, board.Player);
         }
     }
