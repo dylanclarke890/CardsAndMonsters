31261abfc99ae9f94eb6b27ca8ab0c9f8b2a3711
dylanclarke890 <dylanclarke890@gmail.com>
2022-01-09 01:10:48 +0000

    added data tests project with factory tests

---
+++ CardsAndMonsters.Data.Tests/Factories/DuelistFactoryTests.cs
@@ -0,0 +1,57 @@
+﻿using CardsAndMonsters.Data.Factories;
+using Moq;
+using Xunit;
+
+namespace CardsAndMonsters.Data.Tests.Factories
+{
+    public class DuelistFactoryTests
+    {
+        private readonly MockRepository _mockRepository;
+
+
+
+        public DuelistFactoryTests()
+        {
+            _mockRepository = new MockRepository(MockBehavior.Strict);
+
+
+        }
+
+        private DuelistFactory CreateFactory()
+        {
+            return new DuelistFactory();
+        }
+
+        [Fact]
+        public void GetNewPlayer_StateUnderTest_ExpectedBehavior()
+        {
+            // Arrange
+            var factory = CreateFactory();
+
+            // Act
+            var result = factory.GetNewPlayer();
+
+            // Assert
+            Assert.Equal("Player", result.Name);
+            Assert.NotEmpty(result.Deck);
+
+            _mockRepository.VerifyAll();
+        }
+
+        [Fact]
+        public void GetNewOpponent_StateUnderTest_ExpectedBehavior()
+        {
+            // Arrange
+            var factory = CreateFactory();
+
+            // Act
+            var result = factory.GetNewOpponent();
+
+            // Assert
+            Assert.Equal("Opponent", result.Name);
+            Assert.NotEmpty(result.Deck);
+
+            _mockRepository.VerifyAll();
+        }
+    }
+}
