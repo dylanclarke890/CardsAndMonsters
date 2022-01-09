9118e9c8b07aaae3d050deaec6cf6de4c103be7d
dylanclarke890 <dylanclarke890@gmail.com>
2022-01-09 14:52:18 +0000

    added more app constants

--- CardsAndMonsters.Core/AppConstants.cs
+++ CardsAndMonsters.Core/AppConstants.cs
@@ -3,7 +3,11 @@
     public static class AppConstants
     {
         public static readonly int FieldSize = 3;
-        
+
         public static readonly int HandSize = 5;
+
+        public static readonly int DeckSize = 40;
+
+        public static readonly int NormalSummonLimit = 1;
     }
 }
