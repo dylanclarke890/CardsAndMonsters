33d1c85eae66ec84db86aae66e93842b137e3711
dylanclarke890 <dylanclarke890@gmail.com>
2022-01-09 12:25:51 +0000

    added extra test to board tests

--- CardsAndMonsters.Models.Tests/BoardTests.cs
+++ CardsAndMonsters.Models.Tests/BoardTests.cs
@@ -7,7 +7,7 @@ namespace CardsAndMonsters.Models.Tests
     public class BoardTests
     {
         [Fact]
-        public void AbleToPlayMonster_ValidSetup_ReturnsTrue()
+        public void AbleToPlayMonster_WhenAbleToPlay_ReturnsTrue()
         {
             // Arrange
             Monster monster = new(100, 100);
@@ -23,11 +23,27 @@ namespace CardsAndMonsters.Models.Tests
             Assert.True(result);
         }
 
+        [Fact]
+        public void AbleToPlayMonster_WhenHandDoesNotContainMonster_ReturnsFalse()
+        {
+            // Arrange
+            Monster monster = new(100, 100);
+            Duelist player = new("test");
+            Board board = new() { Player = player };
+            board.CurrentTurn = new(player) { Phase = Phase.Main };
+
+            // Act
+            var result = board.AbleToPlayMonster(monster);
+
+            // Assert
+            Assert.False(result);
+        }
+
         [Theory]
         [InlineData(Phase.Standby)]
         [InlineData(Phase.Battle)]
         [InlineData(Phase.End)]
-        public void AbleToPlayMonster_InvalidPhase_ReturnsFalse(Phase phase)
+        public void AbleToPlayMonster_WhenPhaseIsNotMain_ReturnsFalse(Phase phase)
         {
             // Arrange
             Monster monster = new(100, 100);
